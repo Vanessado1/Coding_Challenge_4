39fa5eca929e9d2bd99d5f74069e3c2b89e4906d
Vanessa Do <xex.vanessa@gmail.com>
2025-02-02 01:36:49 -0500

    Task 2- Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,3 +6,11 @@ if (purchaseAmount > 100) {
     total = purchaseAmount * discount 
 }; // If statement to make sure the discount can be applied to total 
 console.log(`Discounted Total: $${total}`); 
+
+// Task 2- For Loop
+let sales = [ 350, 189, 26, 12, 86]; //Declared sales array
+let totalSales= 0; //Declared total sales 
+for (let index = 0; index < sales.length; index++) {
+    totalSales += sales[index];
+} // For loop used to add all sales together 
+console.log(`Total Sales: $${totalSales}`); 
