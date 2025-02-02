dd4bdeae976acc52022b5eb2fbf143b1faf54b15
Vanessa Do <xex.vanessa@gmail.com>
2025-02-02 01:21:20 -0500

    Task 1- Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+// Task 1- If Statements 
+let purchaseAmount = 150; // Declared purchase amount 
+let discount = 0.15; // Declared discount amount 
+let total; // Final amount after discount is applied 
+if (purchaseAmount > 100) {
+    total = purchaseAmount * discount 
+}; // If statement to make sure the discount can be applied to total 
+console.log(`Discounted Total: $${total}`); 
